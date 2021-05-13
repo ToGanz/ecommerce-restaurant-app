c7dfa49e0cd95e66ab4d71bd9a3c1efd2d7b9739
tganz <tganz5890@googlemail.com>
2021-05-13 05:13:35 +0200

    add category reference to product with not null validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_025154) do
+ActiveRecord::Schema.define(version: 2021_05_13_030613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_05_13_025154) do
     t.boolean "featured", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["name"], name: "index_products_on_name", unique: true
   end
 
@@ -50,4 +52,5 @@ ActiveRecord::Schema.define(version: 2021_05_13_025154) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
