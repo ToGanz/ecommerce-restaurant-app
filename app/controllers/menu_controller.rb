7874e46108b2f2e13c93ddf79fa70e2c09b0fc94
tganz <tganz5890@googlemail.com>
2021-05-14 04:47:26 +0200

    set categories in menu

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,14 @@
 class MenuController < ApplicationController
+  before_action :set_categories
+  
   def index
     @page = 'menu'
     @products = Product.all
+  end
+
+  private
+
+  def set_categories
+    @categories = Category.all.where(display: true)
   end
 end
