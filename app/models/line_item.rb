64c34ab509059f5c721db3b3cf2272d2fae4d297
tganz <tganz5890@googlemail.com>
2021-05-15 06:12:36 +0200

    set unit price  and total before saving line item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,23 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  before_save :set_unit_price
+  before_save :set_total
+
+  private
+
+  def set_unit_price
+    # is there already is a record, dont the the unit price
+    if persisted?
+      self[:unit_price]
+    else
+      self[:unit_price] = product.price
+    end
+  end
+
+  def set_total
+    self[:total] = unit_price * quantity
+  end
+
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe LineItem, type: :model do
       expect(subject).to be_valid
     end
   end
+
+  describe 'before save' do
+    subject { FactoryBot.create(:line_item) }
+
+    context '#set_unit_price' do
+
+      it 'unit price is eq to product price' do
+        expect(subject.unit_price).to eq(subject.product.price)
+      end
+
+      it 'unit price stays the same after save, even though product price changes' do
+        subject.product.update(price: 12)
+        expect(subject.unit_price).not_to eq(subject.product.price)
+      end
+
+    end
+
+    context '#set_total' do
+
+      it 'set total is correct' do
+        expect(subject.total).to eq(subject.product.price * subject.quantity)
+      end
+
+    end
+
+  end
 end
