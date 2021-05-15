5c998efdc0ba44758e0ade853c960cbc1ca78874
tganz <tganz5890@googlemail.com>
2021-05-15 06:15:00 +0200

    add total to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_035417) do
+ActiveRecord::Schema.define(version: 2021_05_15_041322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_035417) do
     t.boolean "canceled", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "total", default: "0.0"
   end
 
   create_table "products", force: :cascade do |t|
