d0fa7380a2e86a800774e6b300930b549970693a
tganz <tganz5890@googlemail.com>
2021-05-15 07:08:25 +0200

    add cart controller and current_order  helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def current_order
+    # Use find by id avoid potential errors
+    # Order.find sometimes doesnt work with .nil?
+    if Order.find_by_id(session[:order_id]).nil?
+      Order.new
+    else
+      Order.find_by_id(session[:order_id])
+    end
+  end
 end
