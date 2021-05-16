9d4d911a313408d61ea2317a507e383357393d18
tganz <tganz5890@googlemail.com>
2021-05-16 08:42:39 +0200

    fix pluralize in cart link

--- spec/features/cart/adding_to_cart_spec.rb
+++ spec/features/cart/adding_to_cart_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Adding to acart', type: :feature do
-  before { FactoryBot.create(:product) }
 
   scenario 'add a product' do
+    product = FactoryBot.create(:product)
     visit menu_path
     click_on 'Add to Cart'
-    sleep(5)
-    click_on 'View Cart'
+
+    click_on "1 item with a value of #{product.price} €"
 
     expect(page).to have_content(product.name)
     expect(page).to have_content("Total: #{product.price}")
