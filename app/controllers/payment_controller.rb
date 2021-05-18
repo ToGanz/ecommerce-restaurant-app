a48b0e4a408da9755236dbe0a16726b87528d2e9
tganz <tganz5890@googlemail.com>
2021-05-18 06:40:50 +0200

    fix return  value for webhook

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -52,10 +52,10 @@ class PaymentController < ApplicationController
       event = Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
     rescue JSON::ParserError => e
       # Invalid payload
-      return status 400
+      return nothing: true, status: :bad_request
     rescue Stripe::SignatureVerificationError => e
       # Invalid signature
-      return status 400
+      return nothing: true, status: :bad_request
     end
 
     if event['type'] == 'checkout.session.completed'
