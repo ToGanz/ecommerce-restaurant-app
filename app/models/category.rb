d919d1ed154842a5c3efd8850b82ec3f1b6acb33
tganz <tganz5890@googlemail.com>
2021-05-19 10:00:04 +0200

    add menu styling

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :featured_products, -> { Product.featured }, class_name: 'Product', inverse_of: :category
   
   validates :name, presence: true, uniqueness: true
   validates :description, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed" }
